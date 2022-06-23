715057994deb1f1bc0a72975fba76ac35a678e36
Roopa S. Prabha <roopa.prabha@claysys.com>
2022-06-23 12:35:04 +0530

    FirstMVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gitAzureDevopes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
